b1771e2b3aee2273a8547717fd9bed5737ded9c4
ArhyParh <zescoarty@gmail.com>
2023-09-26 16:08:16 +0300

    Работа над ботом закончена

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sqlite3 import Cursor
 
+import requests
 import telebot
 import random
 import sqlite3 as sq
@@ -11,21 +12,14 @@ cur.execute("""CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     proname TEXT,
-    sex INTEGER,
     old INTEGER,
     phone_number INTEGER,
     adress TEXT
 )""")
 
-def print_data_baze():
-    cur.execute("SELECT * FROM users")
-    print(cur.fetchall())
-    for row in cur.fetchall():
-        print(row)
-
-# cur.execute("""INSERT INTO users(name,proname,sex,old,phone_number,adress)
-# VALUES ('Артем','Пархоменко',1,27,79006501355,'Санкт-Петербург')""")
-# cur.execute("SELECT * FROM users")
+# cur.execute("""INSERT INTO users(name,proname,old,phone_number,adress)
+# VALUES ('Артем','Пархоменко',27,79006501355,'Санкт-Петербург')""")
+# #cur.execute("SELECT * FROM users")
 
 API_TOKEN = '6374135776:AAEjQJnGdbvwGwKYjH1oA2J93tV6Kzo__Mc'
 bot = telebot.TeleBot(API_TOKEN)
@@ -41,12 +35,67 @@ def start_message(message):
 
 @bot.message_handler(commands=['add'])
 def add_person(message):
-    bot.send_message(message.chat.id,"Введите данные нового пользователя по примеру:"
-                                     "Артем Пархоменко 1 27 79006501355 Санкт-Петербург")
+    list_i = message.text.split()[1:]
+    try:
+        cur.execute("""INSERT INTO users(name,proname,old,phone_number,adress)
+        VALUES (?,?,?,?,?)""",list_i)
+        con.commit()
+        bot.send_message(message.chat.id,"Запись успешно добавлена в телефонный справочник")
+    except:
+        bot.send_message(message.chat.id,"Введите данные нового пользователя по примеру:"
+                                         "/add Артем Пархоменко 27 79006501355 Санкт-Петербург")
 
 
-print_data_baze()
-con.commit()
-# con.c
+@bot.message_handler(commands=['delete'])
+def add_person(message):
+    list_i = message.text.split()[1:]
+    try:
+        cur.execute("""DELETE FROM users WHERE name = ? and proname = ?""",list_i)
+        bot.send_message(message.chat.id,"Запись успешно удалена")
+    except:
+        bot.send_message(message.chat.id, "Формат записи для удаления: /delete Артем Пархоменко")
+
+
+# @bot.message_handler(commands=['update'])
+# def add_person(message):
+#     list_i = message.text.split()[1:]
+#     list_update = list_i[:2]
+#     list_delete = list_i[2:]
+    # try:
+    # sql1 = ("""UPDATE users SET(?,?)""",list_update)
+    # sql2 = ("""WHERE name = ? and proname = ?""",list_delete)
+    # cur.execute(sql1+sql2)
+    # cur.execute("""UPDATE users SET (?,?) WHERE name  and proname =""",list_i)
+
+    # bot.send_message(message.chat.id,"Запись успешно изменена")
+    # except:
+    #     bot.send_message(message.chat.id, "Формат записи для удаления: /update Артем Пархоменко Евгений Гайдарович")
+@bot.message_handler(commands=['exit'])
+def add_person(message):
+    bot.send_message(message.chat.id, "Бот завершил свою работу.")
+    con.commit()
+    con.close()
+    bot.polling()
+    bot.stop_bot()
+
+
+@bot.message_handler(commands=['save'])
+def add_person(message):
+    bot.send_message(message.chat.id, "Таблица с номерами сохранена.")
+    con.commit()
+
+
+bot.message_handler(commands=['help'])
+def add_person(message):
+    bot.send_message(message.chat.id, "Для того, чтобы бот вывел текущий справочник номеров введите команду /start."
+                                      "Для добавления записи используйте команду /add"
+                                      "Для удаления записи используйте команду /delete"
+                                      "для сохранения таблицы используйте команду /save"
+                                      "для закрытия и прекращения работы бота используйте команду /exit")
+
+
+
+# print_data_baze()
+# con.commit()
 # con.close()
 bot.polling()
